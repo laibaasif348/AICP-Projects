a34509d8be8ffd83cd99d94983c6c0d4a3bd2af6
laibaasif348 <158566032+laibaasif348@users.noreply.github.com>
2024-03-16 19:35:05 +0500

    Add files via upload

---
+++ project week 6.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Ticket
+{
+    protected:
+    string type;
+    float oneDayPrice;
+    float twoDaysPrice;
+    public:
+    Ticket(string t,float oneDay,float twoDays):type(t),oneDayPrice(oneDay),twoDaysPrice(twoDays) 
+    {
+    }
+    virtual void displayOptions() 
+    {
+        cout<<type<<":\n";
+        cout<<"One day: $"<<oneDayPrice<<endl;
+        cout<<"Two days: $"<<twoDaysPrice<<endl;
+    }
+    float getOneDayPrice() 
+    const 
+    {
+        return oneDayPrice;
+    }
+    float getTwoDaysPrice()
+    const
+    {
+        return twoDaysPrice;
+    }
+};
+class GroupTicket:public Ticket
+{
+    public:
+    GroupTicket():Ticket("Group (6+ people)", 15.00, 22.50)
+    {
+    }
+};
+class FamilyTicket:public Ticket 
+{
+    public:
+    FamilyTicket():Ticket("Family(up to 2 adult& 3 children)",60.00, 90.00) 
+    {
+    }
+};
+class IndividualTicket:public Ticket 
+{
+    public:
+    IndividualTicket(string t,float oneDay,float twoDays):Ticket(t,oneDay,twoDays)
+    {
+    }
+};
+class Attraction
+{
+    protected:
+    string name;
+    float price;
+    public:
+    Attraction(string n,float p):name(n),price(p) 
+    {
+    }
+    void display() 
+    {
+        cout<<name<<": $"<<price<<endl;
+    }
+    float getPrice() 
+    const 
+    {
+        return price;
+    }
+};
+class Booking 
+{
+    private:
+    Ticket* tickets[10];
+    Attraction* attractions[10];
+    int ticketCount;
+    int attractionCount;
+    float totalCost;
+    public:
+    Booking():ticketCount(0),attractionCount(0),totalCost(0) 
+    {
+    }
+    void addTicket(Ticket* ticket) 
+    {
+        if(ticketCount<10) 
+        {
+            tickets[ticketCount++]=ticket;
+        }
+    }
+    void addAttraction(Attraction* attraction) 
+    {
+        if(attractionCount<10) 
+        {
+            attractions[attractionCount++]=attraction;
+        }
+    }
+    float calculateTotalCost() 
+    {
+        totalCost=0;
+        for(int i=0; i<ticketCount;i++) 
+        {
+            totalCost+=tickets[i]->getOneDayPrice();
+        }
+        for(int i=0; i<attractionCount; i++) 
+        {
+            totalCost+=attractions[i]->getPrice();
+        }
+        return totalCost;
+    }
+    void displayBookingDetails() 
+    {
+        cout<<"Booking details:\n";
+        for(int i=0;i<ticketCount;i++) 
+        {
+            tickets[i]->displayOptions();
+        }
+        for (int i=0;i<attractionCount;i++) 
+        {
+            attractions[i]->display();
+        }
+        cout<<"Total Cost: $"<<totalCost<<endl;
+    }
+};
+
+int main() 
+{
+    cout<<"One-day ticket options:\n";
+    IndividualTicket adult("Adult", 20.00, 30.00);
+    IndividualTicket child("Child", 12.00, 18.00);
+    IndividualTicket senior("Senior", 16.00, 24.00);
+    adult.displayOptions();
+    child.displayOptions();
+    senior.displayOptions();
+    GroupTicket group;
+    group.displayOptions();
+    FamilyTicket family;
+    family.displayOptions();
+    
+    cout << "\nAvailable attractions:\n";
+    Attraction lionFeeding("Lion Feeding", 2.50);
+    Attraction penguinFeeding("Penguin Feeding", 2.00);
+    Attraction eveningBarbecue("Evening Barbecue (Two-day tickets only)", 5.00);
+    lionFeeding.display();
+    penguinFeeding.display();
+    eveningBarbecue.display();
+    
+    Booking booking;
+    booking.addTicket(&adult);
+    booking.addTicket(&child);
+    booking.addAttraction(&lionFeeding);
+    booking.addAttraction(&eveningBarbecue);
+    float totalCost = booking.calculateTotalCost();
+    cout<<"\nTotal cost of the booking: $"<<totalCost<<endl;
+
+    booking.displayBookingDetails();
+    return 0;
+}
+
